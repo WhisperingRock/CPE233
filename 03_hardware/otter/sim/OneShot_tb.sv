370a72da65ef2486bfc29a78ebc182f2a4c5ed43
WhisperingRock <xpropellorheadx@gmail.com>
2026-08-23 17:00:52 -0700

    Ditch asynch button
    - Use 2x-flop synch'd flops over asynch to avoid race (synch) on button
    - _tb updated accordingly.
    - 80ns achieved

--- 03_hardware/otter/sim/OneShot_tb.sv
+++ 03_hardware/otter/sim/OneShot_tb.sv
@@ -31,12 +31,13 @@ module OneShot_tb();
     logic           out; 
     
     // ~~ testing ~~
-    logic [31:0]    testcase; 
+    logic [31:0]    testcase;
+    localparam      p_dur = 80;
     
     
     
     // ~~~~ module instances ~~~~
-    OneShot UUT(
+    OneShot #(p_dur) UUT(
         .IN(in),         // 1'b I
         .CLK(clk),       // 1'b I
         .OUT(out)        // 1'b O
@@ -50,72 +51,108 @@ module OneShot_tb();
         clk <= !clk;
     end
     
-    // ~~~~ exercise/testing ~~~~
+    // ~~~~ testing ~~~~
     initial begin
         testcase    = 0;
         clk         = 1'b1;
         in          = 1'b0;
         
-        #100;               // get the crap out 
+        #100;                           // get the poo out 
 
         
         // ~~ TC1 : No button press = no shooting ~~
         testcase++;
-        $display("%d", testcase);
-        #30; 
-        assert(out === 1'b0) else $error("OUT HIGH but expected LOW");
+        $display("TC %d", testcase);
+        for(int i = 0; i < 30; i++) begin 
+            assert(out === 1'b0) else $error("OUT HIGH but expected LOW");
+        end
         
         
         // ~~ TC2 : mid-clk cycle button press ~~
         testcase++;
-        $display("%d", testcase);
+        $display("TC %d", testcase);
         in = 1'b1; 
         #1; 
         in = 1'b0;
+        #29;    // wait the 30ns for button processing delay
 
-        for(int i = 0; i < (80-2); i++) begin
+        for(int i = 0; i < p_dur; i++) begin
             #1;
             $display("%d", i);
             assert(out === 1'b1) else $error("OUT LOW but expected HIGH");
 
         end
-        #2;     // brings us to a clean 10 increment clk count
+        #1;
         assert(out === 1'b0) else $error("OUT HIGH but expected LOW");
         
         
         
-        // ~~ TC3 : retry button during pulse firing ~~
+        // ~~ TC3 : small blip (not on clk) is ignored as noise) ~~
         testcase++;
-        $display("%d", testcase);
+        $display("TC %d", testcase);
         
         // ~ first button ~
         in = 1'b1; 
         #1; 
         in = 1'b0;
-
-        // ~ first half of pulse ~
+        #29;    // wait the 30ns for button processing delay
+        
+        // ~ first half of pulse is on ~
         for(int i = 0; i < 40; i++) begin
+            #1;
+            $display("%d", i);
+            assert(out === 1'b0) else $error("OUT HIGH but expected LOW");
+        end
+        #9; //readjustment to clk
+        
+
+        // ~~ TC4 : retry button during pulse firing ~~
+        testcase++;
+        $display("TC %d", testcase);
+        
+        // ~ first button ~
+        in = 1'b1; 
+        #1; 
+        in = 1'b0;
+        #29;    // wait the 30ns for button processing delay
+        
+        // ~ first half of pulse is on ~
+        for(int i = 0; i < 14; i++) begin
             #1;
             $display("%d", i);
             assert(out === 1'b1) else $error("OUT LOW but expected HIGH");
         end
-        
-
-        
-        // ~ second half of pulse ~
-        for(int i = 0; i < 19; i++) begin
+        // ~ second half of pulse is still on (regardless of button) ~
+        for(int i = 0; i < 14; i++) begin
             // ~ second pulse ~ 
             in = 1'b1; 
-            #1; 
+            #2; 
             
             $display("%d", i);
             assert(out === 1'b1) else $error("OUT LOW but expected HIGH");
             in = 1'b0;
             #1;
         end
-        #2;     // brings us to a clean 10 increment clk count
+        #24;          
+        assert(out === 1'b1) else $error("OUT LOW but expected HIGH");
+        #1;
         assert(out === 1'b0) else $error("OUT HIGH but expected LOW");
+        #200; 
         
+        
+        // ~~ TC4 : long press ~~
+        testcase++;
+        $display("TC %d", testcase);
+        in = 1'b1;
+        #300; 
+        in = 1'b0;
+        #30;                                        // recognize button propagation
+        for(int i = 0; i < 80; i++) begin
+            $display("%d", i);
+            assert(out === 1'b1) else $error("OUT LOW but expected HIGH");
+            #1;
+        end
+        #80; 
     end
     
     
--- 03_hardware/otter/src/OneShot.sv
+++ 03_hardware/otter/src/OneShot.sv
@@ -20,42 +20,102 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module OneShot(
-    input           IN,
+module OneShot
+#(parameter DUR_CNT = 80)
+(
+    input logic     IN,             // active on falling edge
     input logic     CLK, 
     output logic    OUT
     );
     
     // ~~~~ local params ~~~~
-    localparam      SHOTLEN = 80;
-    localparam      PERIOD  = 10;
-    logic [31:0]    delay;
+    // ~~ state ~~
+    typedef enum logic [1:0] {
+        INACTIVE, 
+        PULSE, 
+        INC
+    } e_state;
+    e_state curr_state, next_state;
     
-  
-    // ~~~~ inits ~~~~
-    initial begin
-        delay       = SHOTLEN;     
-    end
+    // ~~ timing ~~
+    localparam      INC_CNT = 10;
+    logic [31:0]    t_elap_ns, next_t_elap_ns; 
+
+    // ~~ button processing ~~
+    logic in_meta;
+    logic in_sync;
+    logic in_sync_d;
+    logic button_press;
     
-    // ~~~~ asynch button ~~~~
-    always_ff @(negedge IN) begin
-        if(delay == SHOTLEN) begin
-            OUT <= 1'b1;
-        end
-    end
-    
-    // ~~~~ synch delay ~~~~
+    // ~~~~ synch for falling button : two-flop ~~~~
+    /* Note : we introduce some delay in waiting for the CLK and
+    //          flops BUT we avoid (re)assigning the same logic synch
+    //          and asynch for the button (as before).
+    */ 
     always_ff @(posedge CLK) begin
+        in_meta             <= IN;
+        in_sync             <= in_meta; 
+        in_sync_d           <= in_sync; 
+    end
+    assign button_press     = in_sync_d && !in_sync; // HIGH during falling edge
     
-        // ~~ pulse on and decrementing ~~
-        if(OUT == 1'b1) begin
-            delay -= PERIOD; 
-        end
+    // ~~~~ synch state progression ~~~~
+    always_ff @(posedge CLK) begin
+        curr_state  <= next_state; 
+        t_elap_ns   <= next_t_elap_ns; 
+    end
+    
+    always_comb begin
+    
+        // ~~ defaults ~~
+        next_state      = curr_state;
+        next_t_elap_ns  = t_elap_ns;
+        OUT             = 1'b0;
+    
+        case(curr_state)
         
-        if(delay <= 0) begin
-            OUT <= 1'b0; 
-            delay = SHOTLEN; 
-        end
+            INACTIVE:   begin
+                next_t_elap_ns   = 0;
+                if(button_press) begin next_state = PULSE; end   
+            end
+            
+            
+            PULSE:      begin
+                
+                OUT         = 1'b1;
+                
+                if(t_elap_ns >= (DUR_CNT - INC_CNT)) begin
+                    next_state  = INACTIVE;
+                    next_t_elap_ns   = 0;  
+                end
+                
+                else begin 
+                    next_state  = PULSE;
+                    next_t_elap_ns = t_elap_ns + INC_CNT; 
+                end
+            end
+            /*
+            INC:        begin
+                OUT         = 1'b1;
+                
+                if(t_elap_ns >= DUR_CNT) begin
+                    next_state  = INACTIVE;
+                    next_t_elap_ns   = 0;  
+                end
+                
+                else begin
+                    next_state  = PULSE;
+                    next_t_elap_ns = t_elap_ns + INC_CNT;
+                end
+            end
+            */
+    
+            default:    begin
+                OUT         = 1'b0;
+                next_state  = INACTIVE;
+                next_t_elap_ns   = 0; 
+            end
+        endcase
     end
     
 endmodule
